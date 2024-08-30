a70c9b833c466e048c4027db64990b229713972c
Torbjörn Törnkvist <kruskakli@gmail.com>
2024-08-30 14:41:41 +0200

    Initial project setup

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Chess Site Backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
