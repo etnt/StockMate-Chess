302a7da683d0c334336da9b2b10d58b48b69be6f
Torbjörn Törnkvist <kruskakli@gmail.com>
2024-08-30 15:38:55 +0200

    Able to see a chess board

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Chessboard } from 'react-chessboard';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Chess Board</h1>
+      <div style={{ width: '800px', margin: 'auto' }}>
+        <Chessboard />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/ChessBoard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Chessboard } from 'react-chessboard';
+
+function ChessBoard() {
+  return (
+    <div>
+      <h1>My Chess Game</h1>
+      <Chessboard id="BasicBoard" />
+    </div>
+  );
+}
+
+export default ChessBoard;
